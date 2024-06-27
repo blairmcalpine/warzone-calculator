8326ffee7ec52814da6725cf5533a0881b3c5611
Blair McAlpine <blair.mcalpine77@gmail.com>
2024-06-27 19:06:20 -0400

    Add suspense

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,25 @@
 import Link from "next/link";
+import { Suspense } from "react";
 import { GitHub } from "~/components/icons/github";
 import { Table } from "~/components/table";
 import { Card } from "~/components/ui/card";
 
 export default function HomePage() {
   return (
-    <main className="flex flex-col items-center justify-center">
-      <Card className="flex flex-col gap-8 p-6 md:min-h-fit md:gap-10 md:p-8">
-        <Table />
-        <Link
-          className="text-md flex w-full items-center justify-center gap-3 tracking-tight text-muted-foreground"
-          href="https://github.com/blairmcalpine"
-          target="_blank"
-        >
-          Created by Blair McAlpine
-          <GitHub className="inline-block h-6 w-6" fill="currentColor" />
-        </Link>
-      </Card>
+    <main className="flex flex-col items-center justify-center md:min-h-dvh">
+      <Suspense>
+        <Card className="flex flex-col gap-8 p-6 md:min-h-fit md:gap-10 md:p-8">
+          <Table />
+          <Link
+            className="text-md flex w-full items-center justify-center gap-3 tracking-tight text-muted-foreground"
+            href="https://github.com/blairmcalpine"
+            target="_blank"
+          >
+            Created by Blair McAlpine
+            <GitHub className="inline-block h-6 w-6" fill="currentColor" />
+          </Link>
+        </Card>
+      </Suspense>
     </main>
   );
 }
